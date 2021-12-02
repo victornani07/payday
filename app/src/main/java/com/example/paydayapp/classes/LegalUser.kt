4cc2a5bd50d5586cc0f468da4feb1674b5b10314
DenisaLebanov <lebanovdenisa@yahoo.com>
2021-12-02 18:58:29 +0200

    Legal Person Sign Up Functionalities + Changes

--- app/src/main/java/com/example/paydayapp/classes/LegalUser.kt
+++ app/src/main/java/com/example/paydayapp/classes/LegalUser.kt
@@ -1,10 +1,10 @@
 package com.example.paydayapp.classes
 
-data class NaturalUser(
-    val firstName: String,
-    val lastName: String,
+data class LegalUser (
+    val companyName: String,
+    val cui: String,
     val username: String,
-    val email: String,
+//    val email: String,
     val password: String,
     val domain : String
 )
