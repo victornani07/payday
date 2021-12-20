6ba190221efc21bc5510a9b14a57bb92793836c2
DenisaLebanov <lebanovdenisa@yahoo.com>
2021-12-20 12:49:36 +0200

    Adding Products + Changes

--- app/src/main/java/com/example/paydayapp/LegalPersonSignUpActivity.kt
+++ app/src/main/java/com/example/paydayapp/LegalPersonSignUpActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ImageView
@@ -20,6 +21,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.lang.NullPointerException
 import kotlin.properties.Delegates
 
 class LegalPersonSignUpActivity : AppCompatActivity() {
@@ -167,7 +169,7 @@ class LegalPersonSignUpActivity : AppCompatActivity() {
                         val legalUserId = username + "_legal"
                         val legalUser = LegalUser(companyName, CUI, username, password, "L")
                         refUsers.child(legalUserId).setValue(legalUser)
-                        startActivity(Intent(this@LegalPersonSignUpActivity, LogInActivity::class.java))
+                        startActivity(Intent(this@LegalPersonSignUpActivity, AddProductsActivity::class.java))
                     }
                 }
 
